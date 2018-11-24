3eb932d3a6d0587e750b27d19694a4e138a0af18
javhadxjo <45154044+javhadxjo@users.noreply.github.com>
2018-11-23 20:22:36 -0600

    Update Conexiondb.php

--- Conexiondb.php
+++ Conexiondb.php
@@ -1,11 +1,12 @@
 <?php
 
 function conectarBD(){
-	$host="host=localhost";
+	$host="host=
+ec2-54-225-110-156.compute-1.amazonaws.com";
 	$port="port=5432";
-	$name="dbname=ejemplo";
-	$user="user=postgres";
-	$password="password=barragantijerina";
+	$name="dbname=d1ljg7bt9lemgu";
+	$user="user=tftzfmvpojyrai";
+	$password="password=7eeae106841be7e5d61ce570992cc5ee0dfc084137bb6a43104222b7ff5f0f53";
 	$db = pg_connect("$host $port $name $user $password");
 
 	if(!$db){
